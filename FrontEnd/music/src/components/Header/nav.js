6928b8dbb5381e816089d821955fe4918aa8e121
Neha <nehac.cs19@bmsce.ac.in>
2022-06-06 23:18:28 +0530

    added nav.js

---
+++ FrontEnd/music/src/components/Header/nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const active = { textDecoration: 'none' };
+
+const AppNav=()=> {
+  return (
+    <Navbar collapseOnSelect expand='lg' variant='light' bg='light'>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <NavLink className='nav-link' to='/home' exact activeStyle={active}>
+            Home
+          </NavLink>
+          <NavLink className='nav-link' to='/about' exact activeStyle={active}>
+            About
+          </NavLink>
+          <NavLink className='nav-link' to='/links' exact activeStyle={active}>
+            Links
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+export default AppNav;
